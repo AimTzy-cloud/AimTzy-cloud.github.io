8a1421baf20280b632cd67f6cf5247083fe0aa9b
AimTzy-cloud <aimtzy18@gmail.com>
2024-08-13 03:18:13 +0700

    menambahkan suara

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function welcomeMessage() {
-    const msg = new SpeechSynthesisUtterance();
+if ('speechSynthesis' in window) {
+    let msg = new SpeechSynthesisUtterance();
     msg.text = "Selamat datang di website Baim Junior, Dimana anda bisa belajar tentang web developer dan belajar tool hacking atau cyber keamanan dan kamu juga bisa belajar software developer, Ingat jangan pernah menyerah atau mengeluh karna pada dasarnya penyesalan itu adanya diakhir. Terima kasih";
+    msg.onstart = function() {
+        console.log('Speech started');
+    };
+    msg.onerror = function(event) {
+        console.error('SpeechSynthesisUtterance error:', event.error);
+        alert('Sepertinya fitur suara tidak dapat dijalankan. Pastikan browser Anda mendukung Web Speech API.');
+    };
     window.speechSynthesis.speak(msg);
+} else {
+    alert('Web Speech API tidak didukung di browser ini.');
 }
-
-// Panggil fungsi ketika halaman dimuat
-window.onload = welcomeMessage;
