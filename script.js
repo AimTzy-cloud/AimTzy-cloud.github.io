ee94701852743f661965b651cb9aa26402140a9b
AimTzy-cloud <aimtzy18@gmail.com>
2024-08-16 03:31:29 +0700

    mengubah CSS

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-if ('speechSynthesis' in window) {
-    let msg = new SpeechSynthesisUtterance();
-    msg.text = "Selamat datang di website Baim Junior, Dimana anda bisa belajar tentang web developer dan belajar tool hacking atau cyber keamanan dan kamu juga bisa belajar software developer, Ingat jangan pernah menyerah atau mengeluh karna pada dasarnya penyesalan itu adanya diakhir. Terima kasih";
-    msg.onstart = function() {
-        console.log('Speech started');
-    };
-    msg.onerror = function(event) {
-        console.error('SpeechSynthesisUtterance error:', event.error);
-        alert('Sepertinya fitur suara tidak dapat dijalankan. Pastikan browser Anda mendukung Web Speech API.');
-    };
-    window.speechSynthesis.speak(msg);
-} else {
-    alert('Web Speech API tidak didukung di browser ini.');
-}
+// Toggle Class Active 
+const navbar = document.querySelector('navbar');
+// Ketika menu diklik 
+document.querySelector('#menu').onclick = () => {
+    navbar.classList.toggle('active');
+};
+
